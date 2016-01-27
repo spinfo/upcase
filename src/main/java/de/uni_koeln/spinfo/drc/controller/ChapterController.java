db9226ecdd7068dc376c45f48cdb691b6067438e
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-01-27 17:20:30 +0100

    Render volume chapters into fragment

--- src/main/java/de/uni_koeln/spinfo/drc/controller/ChapterController.java
+++ src/main/java/de/uni_koeln/spinfo/drc/controller/ChapterController.java
@@ -2,14 +2,15 @@ package de.uni_koeln.spinfo.drc.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import de.uni_koeln.spinfo.drc.mongodb.Constans;
 import de.uni_koeln.spinfo.drc.mongodb.DataBase;
 import de.uni_koeln.spinfo.drc.mongodb.data.document.Chapter;
 import de.uni_koeln.spinfo.drc.mongodb.data.document.Volume;
@@ -17,18 +18,19 @@ import de.uni_koeln.spinfo.drc.mongodb.data.document.Volume;
 @Controller
 @RequestMapping(value = "/drc")
 public class ChapterController {
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private DataBase db;
 
 	@RequestMapping(value = "/chapters", method = RequestMethod.GET)
-	public ModelAndView getChapters(@RequestParam("volumeId") String volumeId) {
+	public String getChapters(@RequestParam("volumeId") String volumeId, Model model) {
 		Volume volume = db.getVolumeRepository().findOne(volumeId);
 		List<Chapter> chapters = db.getChapterRepository().findByVolumeId(volumeId);
-		ModelAndView mv = new ModelAndView(Constans.CHAPTERS);
-		mv.addObject("chapters", chapters);
-		mv.addObject("volume", volume);
-		return mv;
+		model.addAttribute("chapters", chapters);
+		model.addAttribute("volume", volume);
+		return "fragments/chapter-list :: chapter-list";
 	}
 
 }
