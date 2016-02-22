59b5d1e9951803291ca8da61733709bd0f736bd7
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-02-22 17:54:52 +0100

    Add hash, equals and toString methods

--- src/main/java/de/uni_koeln/spinfo/drc/mongodb/data/light/RectangleLight.java
+++ src/main/java/de/uni_koeln/spinfo/drc/mongodb/data/light/RectangleLight.java
@@ -58,6 +58,49 @@ public class RectangleLight {
 	public void setId(String id) {
 		this.id = id;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + height;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + width;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RectangleLight other = (RectangleLight) obj;
+		if (height != other.height)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (width != other.width)
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RectangleLight [x=" + x + ", y=" + y + ", width=" + width
+				+ ", height=" + height + "]";
+	}
 	
 	
 
