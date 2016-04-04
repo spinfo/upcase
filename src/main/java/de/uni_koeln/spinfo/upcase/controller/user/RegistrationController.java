40fb873f38d3c5a89fa183b73c06da024c851c9a
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-04-04 11:41:30 +0200

    Save user after form validation

--- src/main/java/de/uni_koeln/spinfo/upcase/controller/user/RegistrationController.java
+++ src/main/java/de/uni_koeln/spinfo/upcase/controller/user/RegistrationController.java
@@ -1,21 +1,28 @@
-package de.uni_koeln.spinfo.drc.controller;
+package de.uni_koeln.spinfo.upcase.controller.user;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import de.uni_koeln.spinfo.drc.model.RegistrationForm;
-import de.uni_koeln.spinfo.drc.model.RegistrationFormValidator;
+import de.uni_koeln.spinfo.upcase.model.RegistrationForm;
+import de.uni_koeln.spinfo.upcase.model.RegistrationFormValidator;
+import de.uni_koeln.spinfo.upcase.mongodb.data.document.UpcaseUser;
+import de.uni_koeln.spinfo.upcase.mongodb.repository.UpcaseUserRepository;
 
 @Controller
 public class RegistrationController {
-	
+
+	@Autowired
+	private UpcaseUserRepository upcaseUserRepository;
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.setValidator(new RegistrationFormValidator());
@@ -29,11 +36,15 @@ public class RegistrationController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String uploadFiles(@Valid RegistrationForm registrationForm,
+	public String uploadFiles(@Valid @ModelAttribute("registrationForm") RegistrationForm registrationForm,
 			BindingResult bindingResult) {
+
 		if (bindingResult.hasErrors()) {
 			return "signup";
 		}
+
+		upcaseUserRepository.save(new UpcaseUser(registrationForm));
+		// TODO: Security - USER_ROLE
 		return "redirect:/userCollection";
 	}
 
