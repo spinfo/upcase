0b5331b9b5618f8c078896f4dae87e1281f6f24b
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-06-30 10:10:02 +0200

    Add perspective java based app config

---
+++ src/main/java/de/uni_koeln/spinfo/upcase/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package de.uni_koeln.spinfo.upcase.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer /*extends AbstractSecurityWebApplicationInitializer*/ {
+
+}
