48d806e8509f1a68cda62ef5b198da218ad5c44b
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-06-30 10:02:15 +0200

    Restrict password length and check pw confirmation

--- src/main/java/de/uni_koeln/spinfo/upcase/model/validator/RegistrationFormValidator.java
+++ src/main/java/de/uni_koeln/spinfo/upcase/model/validator/RegistrationFormValidator.java
@@ -1,11 +1,19 @@
-package de.uni_koeln.spinfo.upcase.model;
+package de.uni_koeln.spinfo.upcase.model.validator;
 
 import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import de.uni_koeln.spinfo.upcase.model.RegistrationForm;
+
+@Service
 public class RegistrationFormValidator implements Validator {
+
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -14,7 +22,9 @@ public class RegistrationFormValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		
+
+		logger.info(((RegistrationForm) target).toString());
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "NotBlank.registrationForm.firstName");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "NotBlank.registrationForm.lastName");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotBlank.registrationForm.email");
@@ -22,14 +32,19 @@ public class RegistrationFormValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword", "NotBlank.registrationForm.confirmPassword");
 
 		RegistrationForm form = (RegistrationForm) target;
-		
+
 		EmailValidator emailValidator = new EmailValidator();
-		if(!emailValidator.isValid(form.getEmail(), null)) {
+		if (!emailValidator.isValid(form.getEmail(), null)) {
 			errors.rejectValue("email", "Invalid.registrationForm.email");
 		}
-		
-		if (!form.getConfirmPassword().equals(form.getPassword())) {
-			errors.rejectValue("confirmPassword", "NotEqual.registrationForm.confirmPassword");
+
+		if (form.getConfirmPassword() != null && form.getPassword() != null) {
+			if(form.getPassword().length() < 5) {
+				errors.rejectValue("password", "Length.registrationForm.password");
+			}
+			if (!form.getConfirmPassword().equals(form.getPassword())) {
+				errors.rejectValue("confirmPassword", "NotEqual.registrationForm.confirmPassword");
+			}
 		}
 	}
 
